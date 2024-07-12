ab1d61b51ac84477618472ce279ceb53bb9bf328
Samir Akhmed <warrior.owl@yandex.ru>
2024-07-12 13:24:39 +0300

    Change structure of events

--- payment-api/src/models/events/payment.py
+++ payment-api/src/models/events/payment.py
@@ -6,13 +6,12 @@ from pydantic import BaseModel
 
 class PaymentEventABC(BaseModel):
     payment_id: UUID
-    user_id: UUID
-    license_id: UUID
     created_at: datetime.datetime
 
 
 class PaymentCreatedEvent(PaymentEventABC):
-    ...
+    user_id: UUID
+    license_id: UUID
 
 
 class PaymentCanceledEvent(PaymentEventABC):
