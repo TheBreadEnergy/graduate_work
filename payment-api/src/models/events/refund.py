1c24ca8ee20f2cd21047ef8a020885fca999d179
Samir Akhmed <warrior.owl@yandex.ru>
2024-07-12 13:25:05 +0300

    Change structure of events

--- payment-api/src/models/events/refund.py
+++ payment-api/src/models/events/refund.py
@@ -6,13 +6,11 @@ from pydantic import BaseModel
 
 class RefundEventABC(BaseModel):
     refund_id: UUID
-    payment_id: UUID
-    user_id: UUID
     created_at: datetime.datetime
 
 
 class RefundCreatedEvent(RefundEventABC):
-    ...
+    user_id: UUID
 
 
 class RefundCancelledEvent(RefundEventABC):
