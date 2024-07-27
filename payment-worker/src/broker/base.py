0ead6a8e16f1076a18eb23490ddcf36ebd69536a
TheBreadEnergy <thebreadenergy@gmail.com>
2024-07-27 16:51:44 +0300

    Add subscription-scheduler

--- payment-worker/src/broker/base.py
+++ payment-worker/src/broker/base.py
@@ -8,9 +8,6 @@ class AbstractKafkaClient(ABC):
         self.logger = setup_logger(self.__class__.__name__)
 
     @abstractmethod
-    def create_topics(self): ...
-
-    @abstractmethod
     def consume_messages(self): ...
 
 
--- payment-worker/src/main.py
+++ payment-worker/src/main.py
@@ -1,11 +1,17 @@
+import asyncio
+
 from broker.kafka_client import KafkaClient
 from core.config import TOPICS, settings
 
-if __name__ == "__main__":
+
+async def main():
     kafka_client = KafkaClient(
         bootstrap_servers=settings.kafka_broker_host,
         client_id=settings.worker_id,
         topic_names=TOPICS,
     )
-    kafka_client.create_topics()
-    kafka_client.consume_messages()
+    await kafka_client.consume_messages()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
